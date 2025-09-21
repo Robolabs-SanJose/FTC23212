add3c8b7c17229a450a42a3484851d552f67c442
Robolabs-SanJose <robolabssanjose@gmail.com>
2025-09-21 13:41:49 -0700

    Update MecanumDrive.java

--- road-runner-quickstart/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ road-runner-quickstart/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -71,7 +71,6 @@ public final class MecanumDrive {
         public double kS =  0.6159816781434753;
         public double kV =  0.0005914719796852058;
         public double kA = 0;
-
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
         public double minProfileAccel = -30;
